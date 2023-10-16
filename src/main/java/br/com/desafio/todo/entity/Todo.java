8a301066fdee002efe0d3d5ea715ad0cf8b638aa
SmartCrm <nossocrmimob@gmail.com>
2023-10-16 19:22:18 -0300

    entity repository

---
+++ src/main/java/br/com/desafio/todo/entity/Todo.java
@@ -0,0 +1,52 @@
+package br.com.desafio.todo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tododesafio")
+public class Todo {
+    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String descricao;
+    private boolean realizado;
+    private int prioridade;
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public boolean isRealizado() {
+        return realizado;
+    }
+    public void setRealizado(boolean realizado) {
+        this.realizado = realizado;
+    }
+    public int getPrioridade() {
+        return prioridade;
+    }
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+
+}
